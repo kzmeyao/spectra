00b6b104e5a64214c0bf55d57412c22ae0bc155f
Kevin Yao <kzmeyao@gmail.com>
2013-10-12 20:32:11 -0700

    Use step animation to support firefox

--- gradient-wall.js
+++ gradient-wall.js
@@ -1,10 +1,32 @@
 function gradientWall($elem, colors, freq) {
     
-    var animateBackward = function(){
-        $elem.animate({'background-position-y': '0%'}, freq, 'linear', function(){animateForward()});
+   var animateBackward = function(){
+        $elem.animate({
+            'border-spacing': 0
+        },
+        {
+             step: function(now, fx) {
+                $(fx.elem).css("background-position", "0% " + now + "%");
+             },
+             complete: function() {
+             	animateForward();
+             },
+             duration: freq
+        });
     }
     var animateForward = function(){
-        $elem.animate({'background-position-y': "100%"}, freq, 'linear', function(){animateBackward();});
+        $elem.animate({
+            'border-spacing': 100
+        },
+        {
+             step: function(now, fx) {
+                $(fx.elem).css("background-position", "0% " + now + "%");
+             },
+             complete: function() {
+             	animateBackward();
+             },
+             duration: freq
+        });
     }
     
     gradText = "linear-gradient(" + colors.toString() + ")";
