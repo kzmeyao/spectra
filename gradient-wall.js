dc8d5ceba56f1838f2c7027d1c729dbef22f2e78
Kevin Yao <kzmeyao@gmail.com>
2013-10-12 19:22:36 -0700

    script file

---
+++ gradient-wall.js
@@ -0,0 +1,21 @@
+function gradientWall($elem, colors, freq) {
+    
+    var animateBackward = function(){
+        $elem.animate({'background-position-y': '0%'}, freq, 'linear', function(){animateForward()});
+    }
+    var animateForward = function(){
+        $elem.animate({'background-position-y': "100%"}, freq, 'linear', function(){animateBackward();});
+    }
+    
+    gradText = "linear-gradient(" + colors.toString() + ")";
+    $.each(['', '-o-', '-moz-', '-webkit-', '-ms-'], function() {
+        $elem.css({ 'background': this + gradText });
+    });
+    $elem.css({'background-size' : '1% ' + colors.length * 100 + '%'});
+    animateForward();
+}
+
+var defaultColors = ['#87FC70', '#55EFCB', '#5BCAFF', '#1AD6FD'];           
+var defaultFreq = 5000;
+   
+gradientWall($('#your-container'), defaultColors, defaultFreq);
